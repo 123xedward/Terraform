64a45dc8d8e50de44d999f7de75205cdc7ea438b
Edward Montes Hernádez <edward.montesh@gmail.com>
2018-09-03 21:47:08 -0500

    Add credentials
    Add Credentials file

--- wordpress/main.tf
+++ wordpress/main.tf
@@ -1,6 +1,7 @@
 provider "aws" {
   region  = "${var.aws_region}"
   profile = "${var.aws_profile}"
+  shared_credentials_file = "/Users/Edward/.aws/credentials"
 }
 
 #data "aws_availability_zones" "available" {}
--- wordpress/terraform.tfvars
+++ wordpress/terraform.tfvars
@@ -1,4 +1,4 @@
 localip       = "104.173.212.11/32"
-aws_profile		= "superhero"
+aws_profile		= "default"
 aws_region		= "us-east-1"
 vpc_cidr      = "10.0.0.0/16"
