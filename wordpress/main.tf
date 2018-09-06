e0deba5a611bbce26d04108017c57383608d5b05
Edward Montes Hernádez <edward.montesh@gmail.com>
2018-09-05 21:38:00 -0500

    Add rds subnets
    Add RDS Subnets

--- wordpress/main.tf
+++ wordpress/main.tf
@@ -165,3 +165,71 @@ resource "aws_vpc_endpoint" "wp_private-s3_endpoint" {
 }
 POLICY
 }
+
+resource "aws_subnet" "wp_rds1_subnet" {
+  vpc_id                  = "${aws_vpc.wp_vpc.id}"
+  cidr_block              = "${var.cidrs["rds1"]}"
+  map_public_ip_on_launch = false
+  availability_zone       = "${data.aws_availability_zones.available.names[0]}"
+
+  tags {
+    Name = "wp_rds1"
+  }
+}
+
+resource "aws_subnet" "wp_rds2_subnet" {
+  vpc_id                  = "${aws_vpc.wp_vpc.id}"
+  cidr_block              = "${var.cidrs["rds2"]}"
+  map_public_ip_on_launch = false
+  availability_zone       = "${data.aws_availability_zones.available.names[1]}"
+
+  tags {
+    Name = "wp_rds2"
+  }
+}
+
+resource "aws_subnet" "wp_rds3_subnet" {
+  vpc_id                  = "${aws_vpc.wp_vpc.id}"
+  cidr_block              = "${var.cidrs["rds3"]}"
+  map_public_ip_on_launch = false
+  availability_zone       = "${data.aws_availability_zones.available.names[2]}"
+
+  tags {
+    Name = "wp_rds3"
+  }
+}
+
+# Subnet Associations
+
+resource "aws_route_table_association" "wp_public_assoc" {
+  subnet_id      = "${aws_subnet.wp_public1_subnet.id}"
+  route_table_id = "${aws_route_table.wp_public_rt.id}"
+}
+
+resource "aws_route_table_association" "wp_public2_assoc" {
+  subnet_id      = "${aws_subnet.wp_public2_subnet.id}"
+  route_table_id = "${aws_route_table.wp_public_rt.id}"
+}
+
+resource "aws_route_table_association" "wp_private1_assoc" {
+  subnet_id      = "${aws_subnet.wp_private1_subnet.id}"
+  route_table_id = "${aws_default_route_table.wp_private_rt.id}"
+}
+
+resource "aws_route_table_association" "wp_private2_assoc" {
+  subnet_id      = "${aws_subnet.wp_private2_subnet.id}"
+  route_table_id = "${aws_default_route_table.wp_private_rt.id}"
+}
+
+resource "aws_db_subnet_group" "wp_rds_subnetgroup" {
+  name = "wp_rds_subnetgroup"
+
+  subnet_ids = ["${aws_subnet.wp_rds1_subnet.id}",
+    "${aws_subnet.wp_rds2_subnet.id}",
+    "${aws_subnet.wp_rds3_subnet.id}",
+  ]
+
+  tags {
+    Name = "wp_rds_sng"
+  }
+}
