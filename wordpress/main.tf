6d8d7dccbe00cf9b09dbc9ce298000ab9e742ec3
Edward Montes Hernádez <edward.montesh@gmail.com>
2018-09-05 21:35:29 -0500

    S3 Endpoint
    S3 Endpoint

--- wordpress/main.tf
+++ wordpress/main.tf
@@ -141,3 +141,27 @@ resource "aws_subnet" "wp_private2_subnet" {
     Name = "wp_private2"
   }
 }
+
+#TODO improve security
+#create S3 VPC endpoint
+resource "aws_vpc_endpoint" "wp_private-s3_endpoint" {
+  vpc_id       = "${aws_vpc.wp_vpc.id}"
+  service_name = "com.amazonaws.${var.aws_region}.s3"
+
+  route_table_ids = ["${aws_vpc.wp_vpc.main_route_table_id}",
+    "${aws_route_table.wp_public_rt.id}",
+  ]
+
+  policy = <<POLICY
+{
+    "Statement": [
+        {
+            "Action": "*",
+            "Effect": "Allow",
+            "Resource": "*",
+            "Principal": "*"
+        }
+    ]
+}
+POLICY
+}
