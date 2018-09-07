bb81889a865fa161eb401ec09de73f27a943689a
Edward Montes Hernádez <edward.montesh@gmail.com>
2018-09-07 07:00:15 -0500

    Security Groups
    Add Security Groups

--- wordpress/main.tf
+++ wordpress/main.tf
@@ -233,3 +233,35 @@ resource "aws_db_subnet_group" "wp_rds_subnetgroup" {
     Name = "wp_rds_sng"
   }
 }
+
+#Security groups
+
+resource "aws_security_group" "wp_dev_sg" {
+  name        = "wp_dev_sg"
+  description = "Used for access to the dev instance"
+  vpc_id      = "${aws_vpc.wp_vpc.id}"
+
+  #SSH
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["${var.localip}"]
+  }
+
+  #HTTP
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["${var.localip}"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
