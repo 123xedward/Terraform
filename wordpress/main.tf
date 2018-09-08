9027e20e7157f3a0e56db2e471cd1df922d298aa
Edward Montes Hernádez <edward.montesh@gmail.com>
2018-09-08 15:47:11 -0500

    Add S3
    Add S3

--- wordpress/main.tf
+++ wordpress/main.tf
@@ -334,3 +334,19 @@ resource "aws_security_group" "wp_rds_sg" {
     ]
   }
 }
+
+#S3 code bucket
+
+resource "random_id" "wp_code_bucket" {
+  byte_length = 2
+}
+
+resource "aws_s3_bucket" "code" {
+  bucket        = "${var.domain_name}-${random_id.wp_code_bucket.dec}"
+  acl           = "private"
+  force_destroy = true
+
+  tags {
+    Name = "code bucket"
+  }
+}
